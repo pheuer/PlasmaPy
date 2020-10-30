42d38b4c7d90b7d433e2fcabed42527c7c3227c6
pheuer <pvheuer@gmail.com>
2020-10-30 15:08:00 -0400

    Update particletracker.py

--- plasmapy/simulation/particletracker.py
+++ plasmapy/simulation/particletracker.py
@@ -7,11 +7,62 @@ import astropy.units as u
 import numpy as np
 import scipy.interpolate as interp
 
-from astropy import constants
+import astropy.constants as const
 
 from plasmapy.particles import atomic
 from plasmapy.utils.decorators import validate_quantities
 
+
+class ParticleSpecies:
+    """
+    Object representing a number of macroparticles of the same species
+    to enter into ParticleTracker
+
+    Parameters
+    ----------
+
+    particle_type : `Particle` instance or str
+        The type of particle that makes up this species
+
+    position : `u.Quantity` array of shape (N,3)
+        Initial positions of each particle
+
+    velocity : `u.Quantity` array of shape (N,3)
+        Initial velocity of each particle
+
+    """
+
+    def __init__(
+        self,
+        particle_type,
+        position,
+        velocity,
+        ):
+
+        if not isinstance(particle, Particle):
+            self.particle = Particle(particle_type)
+        else:
+            self.particle = particle_type
+
+
+        if position.shape != velocity.shape:
+            raise ValueError("Position and Velocity arrays must have the "
+                             "same shape, but entered array shapes are "
+                             f" {position.shape} and {velocity.shape}")
+
+        self.x = position
+        self.v = velocity
+
+        # Number of particles
+        self.N = self.x.shape[0]
+        # Particle charge
+        self.q = self.particle.charge
+        # Particle mass
+        self.m = self.particle.mass
+
+
+
+# TODO: Have the option
 
 class ParticleTracker:
     """
@@ -20,36 +71,39 @@ class ParticleTracker:
 
     Parameters
     ----------
-    plasma : `Plasma`
-        plasma from which fields can be pulled
-    type : str
-        particle type. See `plasmapy.particles.atomic` for suitable arguments.
-        The default is a proton.
-    n_particles : int
-        number of macroparticles. The default is a single particle.
-    scaling : float
-        number of particles represented by each macroparticle.
-        The default is 1, which means a 1:1 correspondence between particles
-        and macroparticles.
+    plasma : 'Plasma3D' object or list of same
+        A list of 'Plasma3D' objects, each of which contains a grid and
+        one or more quantities on the grid.
+
+        This allows different quantities (eg. E and B) to be defined on
+        different grids.
+
+    species : 'Species' object or list of same
+        A list of `Species` objects, each of which describes a number of
+        particles to be tracked.
+
     dt : `astropy.units.Quantity`
         length of timestep
-    nt : int
-        number of timesteps
 
-    Attributes
-    ----------
-    x : `astropy.units.Quantity`
-    v : `astropy.units.Quantity`
-        Current position and velocity, respectively. Shape (n, 3).
-    position_history : `astropy.units.Quantity`
-    velocity_history : `astropy.units.Quantity`
-        History of position and velocity. Shape (nt, n, 3).
-    q : `astropy.units.Quantity`
-    m : `astropy.units.Quantity`
-        Charge and mass of particle.
-    eff_q : `astropy.units.Quantity`
-    eff_m : `astropy.units.Quantity`
-        Total charge and mass of macroparticle.
+    end_condition : Integer or a function that takes a ParticleTracker as input
+        Either an integer number of iterations or a function that takes
+        the ParticleTracker as input and returns a boolean. If provided,
+        this function is evalulated every iteration and the tracker stops
+        once the function returns True.
+
+    recorded_variables  : List of strings
+        A list of strings representing properties either of the particles
+        or defined on the grid. If this keyword is set, at each timestep
+        the value of each of these properties will be recorded for each
+        particle (grid values are interpolated to particle positons).
+
+        If record_file is set, the data is saved there. Otherwise it will be
+        stored in memory.
+
+    record_file : path string
+        A path for an HDF5 file where particle histories will be saved. If
+        None, no histories will be saved.
+
 
     Examples
     ----------
@@ -62,72 +116,47 @@ class ParticleTracker:
     def __init__(
         self,
         plasma,
-        particle_type="p",
-        n_particles=1,
-        scaling=1,
-        dt=np.inf * u.s,
-        nt=np.inf,
-    ):
+        species,
+        dt=None,
+        end_condition = 100,
+        record_file = None,
+        ):
 
-        if np.isinf(dt) and np.isinf(nt):  # coverage: ignore
-            raise ValueError("Both dt and nt are infinite.")
 
-        self.q = atomic.integer_charge(particle_type) * constants.e.si
-        self.m = atomic.particle_mass(particle_type)
-        self.N = int(n_particles)
-        self.scaling = scaling
-        self.eff_q = self.q * scaling
-        self.eff_m = self.m * scaling
+        # Create a dictionary that links quantities on each grid to the
+        # grid they come from.
+        self.quantities = {}
+        for p in plasma:
 
-        self.plasma = plasma
+            # TODO: Somehow deal with the possibility of multiple grids
+            # having the same quantity (which to choose?)
 
-        self.dt = dt
-        self.NT = int(nt)
-        self.t = np.arange(nt) * dt
+            for quantity_key in p.quantities.keys():
+                self.quantities[quantity_key] = p.interpolator
 
-        self.x = np.zeros((n_particles, 3), dtype=float) * u.m
-        self.v = np.zeros((n_particles, 3), dtype=float) * (u.m / u.s)
-        self.name = particle_type
+        self.species = species
 
-        self.position_history = np.zeros((self.NT, *self.x.shape), dtype=float) * u.m
-        self.velocity_history = np.zeros((self.NT, *self.v.shape), dtype=float) * (
-            u.m / u.s
-        )
-        # create intermediate array of dimension (nx,ny,nz,3) in order to allow
-        # interpolation on non-equal spatial domain dimensions
-        _B = np.moveaxis(self.plasma.magnetic_field.si.value, 0, -1)
-        _E = np.moveaxis(self.plasma.electric_field.si.value, 0, -1)
 
-        self._B_interpolator = interp.RegularGridInterpolator(
-            (self.plasma.x.si.value, self.plasma.y.si.value, self.plasma.z.si.value),
-            _B,
-            method="linear",
-            bounds_error=True,
-        )
+        self.end_condition = end_condition
 
-        self._E_interpolator = interp.RegularGridInterpolator(
-            (self.plasma.x.si.value, self.plasma.y.si.value, self.plasma.z.si.value),
-            _E,
-            method="linear",
-            bounds_error=True,
-        )
+        # Counter for the number of interations
+        self.iterations = 0
 
-    def _interpolate_fields(self):
-        interpolated_b = self._B_interpolator(self.x.si.value) * u.T
-        interpolated_e = self._E_interpolator(self.x.si.value) * u.V / u.m
-        return interpolated_b, interpolated_e
 
-    @property
-    def kinetic_energy_history(self):
-        r"""
-        Calculates the kinetic energy history for each particle.
 
-        Returns
-        --------
-        ~astropy.units.Quantity
-            Array of kinetic energies, shape (nt, n).
+
+
+    def _field_interpolators(self):
         """
-        return (self.velocity_history ** 2).sum(axis=-1) * self.eff_m / 2
+        Interpolates E and B fields required for any charged particle pusher
+        """
+
+        # fetch references to the E and B interpolators within the Plasma3D
+        # object(s)
+
+        return b_interpolator, e_interpolator
+
+
 
     def boris_push(self, init=False):
         r"""
@@ -160,106 +189,146 @@ class ParticleTracker:
         .. [1] C. K. Birdsall, A. B. Langdon, "Plasma Physics via Computer
                Simulation", 2004, p. 58-63
         """
+
+        b_interpolator, e_interpolator = self._field_interpolators()
+
+        # TODO: Replace this with a function that calculates an adaptive dt
         dt = -self.dt / 2 if init else self.dt
-        b, e = self._interpolate_fields()
 
-        # add first half of electric impulse
-        vminus = self.v + self.eff_q * e / self.eff_m * dt * 0.5
+        # Apply pusher to each species
+        for s in species:
 
-        # rotate to add magnetic field
-        t = -b * self.eff_q / self.eff_m * dt * 0.5
-        s = 2 * t / (1 + (t * t).sum(axis=1, keepdims=True))
-        vprime = vminus + np.cross(vminus.si.value, t) * u.m / u.s
-        vplus = vminus + np.cross(vprime.si.value, s) * u.m / u.s
+            # Interpolate the E and B fields at each particle position
+            e = e_interpolator(s.x)
+            b = b_interpolator(s.x)
 
-        # add second half of electric impulse
-        v_new = vplus + self.eff_q * e / self.eff_m * dt * 0.5
 
-        self.v = v_new
-        if not init:
-            self.x += self.v * dt
+            # add first half of electric impulse
+            vminus = s.v + s.q / s.m * dt * 0.5
+
+            # rotate to add magnetic field
+            t = -b * s.q / s.m * dt * 0.5
+            u = 2 * t / (1 + (t * t).sum(axis=1, keepdims=True))
+            vprime = vminus + np.cross(vminus.si.value, t)
+            vplus = vminus + np.cross(vprime.si.value, u)
+
+            # add second half of electric impulse
+            v_new = vplus + s.q * e / s.m * dt * 0.5
+
+            s.x += s.v * dt
+
+
+
+    # TODO: Add relativistically correct Boris push algorithm here
+
 
     def run(self):
         r"""
         Runs a simulation instance.
         """
-        self.boris_push(init=True)
-        self.position_history[0] = self.x
-        self.velocity_history[0] = self.v
-        for i in range(1, self.NT):
+
+        while not _end(self):
+            # Push particles
             self.boris_push()
-            self.position_history[i] = self.x
-            self.velocity_history[i] = self.v
 
-    def __repr__(self, *args, **kwargs):
-        return (
-            f"Species(q={self.q:.4e},m={self.m:.4e},N={self.N},"
-            f'name="{self.name}",NT={self.NT})'
-        )
+            # Record variables when required
+            if len(self.recorded_variables) != 0:
+                self._record_variables()
 
-    def __str__(self):  # coverage: ignore
-        return (
-            f"{self.N} {self.scaling:.2e}-{self.name} with "
-            f"q = {self.q:.2e}, m = {self.m:.2e}, "
-            f"{self.saved_iterations} saved history "
-            f"steps over {self.NT} iterations"
-        )
+            # Increment
+            self.iterations += 1
 
-    def plot_trajectories(self):  # coverage: ignore
-        r"""Draws trajectory history."""
-        import matplotlib.pyplot as plt
 
-        from astropy.visualization import quantity_support
-        from mpl_toolkits.mplot3d import Axes3D
+    def _end(self):
 
-        quantity_support()
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection="3d")
-        for p_index in range(self.N):
-            r = self.position_history[:, p_index]
-            x, y, z = r.T
-            ax.plot(x, y, z)
-        ax.set_title(self.name)
-        ax.set_xlabel("$x$ position")
-        ax.set_ylabel("$y$ position")
-        ax.set_zlabel("$z$ position")
-        plt.show()
+        if isinstance(self.end_condition, int):
+            if self.iterations >= self.end_condition:
+                return True
+            else:
+                return False
 
-    def plot_time_trajectories(self, plot="xyz"):  # coverage: ignore
-        r"""
-        Draws position history versus time.
+        # If not an integer, end_condition is a function
+        return self.end_condition(self)
 
-        Parameters
-        ----------
-        plot : str (optional)
-            Enable plotting of position component x, y, z for each of these
-            letters included in `plot`.
+
+
+    def _record_variables(self):
         """
-        import matplotlib.pyplot as plt
+        Records the current particle positions and velocities in memory or
+        and HDF5 file.
+        """
 
-        from astropy.visualization import quantity_support
-        from mpl_toolkits.mplot3d import Axes3D
 
-        quantity_support()
-        fig, ax = plt.subplots()
-        for p_index in range(self.N):
-            r = self.position_history[:, p_index]
-            x, y, z = r.T
-            if "x" in plot:
-                ax.plot(self.t, x, label=f"x_{p_index}")
-            if "y" in plot:
-                ax.plot(self.t, y, label=f"y_{p_index}")
-            if "z" in plot:
-                ax.plot(self.t, z, label=f"z_{p_index}")
-        ax.set_title(self.name)
-        ax.legend(loc="best")
-        ax.grid()
-        plt.show()
 
-    def test_kinetic_energy(self):
-        r"""Test conservation of kinetic energy."""
-        assert np.allclose(
-            self.kinetic_energy_history,
-            self.kinetic_energy_history.mean(),
-            atol=3 * self.kinetic_energy_history.std(),
-        ), "Kinetic energy is not conserved!"
+
+
+
+
+# ****************************************************************************
+# Functions that accept a ParticleTracker and make basic plots
+# ****************************************************************************
+
+
+
+def plot_trajectories(particle_tracker):  # coverage: ignore
+    r"""Draws trajectory history."""
+    import matplotlib.pyplot as plt
+
+    from astropy.visualization import quantity_support
+    from mpl_toolkits.mplot3d import Axes3D
+
+    quantity_support()
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    for p_index in range(self.N):
+        r = self.position_history[:, p_index]
+        x, y, z = r.T
+        ax.plot(x, y, z)
+    ax.set_title(self.name)
+    ax.set_xlabel("$x$ position")
+    ax.set_ylabel("$y$ position")
+    ax.set_zlabel("$z$ position")
+    plt.show()
+
+def plot_time_trajectories(particle_tracker, plot="xyz"):  # coverage: ignore
+    r"""
+    Draws position history versus time.
+
+    Parameters
+    ----------
+    plot : str (optional)
+        Enable plotting of position component x, y, z for each of these
+        letters included in `plot`.
+    """
+    import matplotlib.pyplot as plt
+
+    from astropy.visualization import quantity_support
+    from mpl_toolkits.mplot3d import Axes3D
+
+    quantity_support()
+    fig, ax = plt.subplots()
+    for p_index in range(self.N):
+        r = self.position_history[:, p_index]
+        x, y, z = r.T
+        if "x" in plot:
+            ax.plot(self.t, x, label=f"x_{p_index}")
+        if "y" in plot:
+            ax.plot(self.t, y, label=f"y_{p_index}")
+        if "z" in plot:
+            ax.plot(self.t, z, label=f"z_{p_index}")
+    ax.set_title(self.name)
+    ax.legend(loc="best")
+    ax.grid()
+    plt.show()
+
+
+def kinetic_energy_history(particle_tracker):
+    r"""
+    Calculates the kinetic energy history for each particle.
+
+    Returns
+    --------
+    ~astropy.units.Quantity
+        Array of kinetic energies, shape (nt, n).
+    """
+    return (self.velocity_history ** 2).sum(axis=-1) * self.eff_m / 2
